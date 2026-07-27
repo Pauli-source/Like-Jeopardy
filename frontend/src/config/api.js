20402cdf1e27b7a67b7d426f701e884f67140d15
Pauli-source <p.wiessner@oth-aw.de>
2026-07-27 14:59:15 +0200

    api

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,4 @@
 const API_BASE_URL = import.meta.env.PROD
-  ? "/api"
-  : "http://localhost:5050/api";
-
+? "https://like-jeopardy.onrender.com/api"
+: "http://localhost:5050/api";
 export default API_BASE_URL;
